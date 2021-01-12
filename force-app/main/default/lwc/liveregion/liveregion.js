57f96ae8e3fdb4731460bf60bd1bcab03d310d91
Adam Rodenbeck <arodenbeck@salesforce.com>
2021-01-12 14:52:57 -0500

    Updated the liveregion.html to display the queue array. Added some methods for enqueuing messages, and outputting a specific queue

--- force-app/main/default/lwc/liveregion/liveregion.js
+++ force-app/main/default/lwc/liveregion/liveregion.js
@@ -1,7 +1,7 @@
 import { LightningElement, api } from 'lwc';
 
 export default class AriaLive extends LightningElement {
-
+  @api parentQueue;  // Holds the speechQueue from our container component
   outputQueue(outputQueue) {
     for(let i = 0; i > outputQueue.queue.length; i++) {
       document.getElementById(outputQueue.liveRegion).innerText += outputQueue.queue[i];
